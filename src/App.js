c13061f704ae42cc7ecfc1298545379d8bccfe5e
demna-miqava <demnamikava11@gmail.com>
2022-10-05 20:46:25 +0400

    requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Validation from "./MyComp";
+export const instance = axios.create({
+  baseURL: "http://localhost:3001/users",
+});
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isUserUpdating, setIsUserUpdating] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
+  const getUser = async () => {
+    const { data } = await instance.get("");
+    console.log(data);
+    setUsers(data.data);
+  };
 
-function App() {
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const onDelete = async (id) => {
+    await instance.delete(id);
+    getUser();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Validation
+        setIsUserUpdating={setIsUserUpdating}
+        getUser={getUser}
+        isUserUpdating={isUserUpdating}
+        selectedUser={selectedUser}
+      />
+      {users.map((user) => {
+        return (
+          <div key={user._id}>
+            <h1>{user.firstName}</h1>
+            <button
+              onClick={() => {
+                setIsUserUpdating(true);
+                setSelectedUser(user);
+              }}
+            >
+              edit
+            </button>
+            <button onClick={() => onDelete(user._id)}>delete</button>
+          </div>
+        );
+      })} */}
     </div>
   );
-}
+};
 
 export default App;
