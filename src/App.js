537fb3c001e8a5fc79e11e9e686eaa4209f5954e
demna-miqava <demnamikava11@gmail.com>
2022-10-05 22:22:41 +0400

    dasda

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Checkout from "./Checkout";
 import Validation from "./MyComp";
+import Navbar from "./Navbar";
+import { ProviderForProductContext } from "./ProductContext";
+import ProductList from "./ProductList";
+import { UserContextProvider } from "./UserContext";
 export const instance = axios.create({
   baseURL: "http://localhost:3001/users",
 });
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [isUserUpdating, setIsUserUpdating] = useState(false);
-  const [selectedUser, setSelectedUser] = useState({});
-  const getUser = async () => {
-    const { data } = await instance.get("");
-    console.log(data);
-    setUsers(data.data);
-  };
-
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  const onDelete = async (id) => {
-    await instance.delete(id);
-    getUser();
-  };
-
   return (
     <div>
-      {/* <Validation
-        setIsUserUpdating={setIsUserUpdating}
-        getUser={getUser}
-        isUserUpdating={isUserUpdating}
-        selectedUser={selectedUser}
-      />
-      {users.map((user) => {
-        return (
-          <div key={user._id}>
-            <h1>{user.firstName}</h1>
-            <button
-              onClick={() => {
-                setIsUserUpdating(true);
-                setSelectedUser(user);
-              }}
-            >
-              edit
-            </button>
-            <button onClick={() => onDelete(user._id)}>delete</button>
-          </div>
-        );
-      })} */}
+      <ProviderForProductContext>
+        <UserContextProvider>
+          <Navbar />
+          <ProductList />
+        </UserContextProvider>
+        <Checkout />
+      </ProviderForProductContext>
     </div>
   );
 };
